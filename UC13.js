e875b118902e300d021e8637744b2b15e99251ee
Carolyn GERLACH <EllaGerlach@Carolyns-MacBook-Air.local>
2017-05-02 14:55:44 -0700

    finished speak, eat and nap function

--- UC13.js
+++ UC13.js
@@ -12,10 +12,55 @@ function Cube(length, mass) {
     }
 }
 
-function Dog(name, age, state) {
-    this.name = name;
-    this.age = age;
-    this.state = state;
-}
+//The Pet object also has a sleepy property that is initially 0, a hunger property that is initially zero, and an awake property that is initially true.
 
-var Dog = new Dog("Kona", 11, "Sleepy");
+function Pet(species, name) {
+    this.species = species;
+    this.name = name;
+    this.sleepy = 0;
+    this.hunger = 0;
+    this.awake = true;
+
+//The Pet object has a speak method that returns "Woof! That means I love you!" if the pet is a dog and "Ugh. meow." if the pet is a cat.
+
+    this.sleepy++;
+    this.hunger++;
+
+    this.speak = function() {
+        if(this.species == "Dog") {
+            return "Woof! That means I love you!";
+        }
+        else if(this.species == "Cat") {
+            return "Ugh. Meow.";
+        }
+    //    else{}
+    }
+
+    //The Pet object has an eat method
+
+    this.eat = function() {
+        this.hunger = 0;
+        this.sleepy++;
+
+        if (this.species == "Cat") {
+            return "Ugh, I guess I'll eat this.";
+
+        }
+        else if (this.species == "Dog"); {
+            return "Nom nom nom nom. Yum, thank you for this delicious food! I love you!";
+        }
+
+     //   else{}
+    }
+
+    //The Pet object has a nap method
+
+    this.nap = function() {
+        this.sleepy = 0;
+        this.awake = false;
+        return "Zzzz...";
+    }
+
+    //The Pet object has a wakeUp method
+
+}
