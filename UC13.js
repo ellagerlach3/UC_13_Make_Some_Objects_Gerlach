e31e0168b7049f62038e7665eb6db0b4a6c8968a
Carolyn GERLACH <EllaGerlach@Carolyns-MacBook-Air.local>
2017-05-13 11:20:17 -0700

    all of UC 13

--- UC13.js
+++ UC13.js
@@ -12,55 +12,143 @@ function Cube(length, mass) {
     }
 }
 
-//The Pet object also has a sleepy property that is initially 0, a hunger property that is initially zero, and an awake property that is initially true.
 
-function Pet(species, name) {
+
+
+
+//The Pet object also has a sleepy property that is initially 0, a hunger property that is initially zero, and an awake property that is initially true.
+function Pet(name, species) {
     this.species = species;
     this.name = name;
     this.sleepy = 0;
     this.hunger = 0;
     this.awake = true;
-
-//The Pet object has a speak method that returns "Woof! That means I love you!" if the pet is a dog and "Ugh. meow." if the pet is a cat.
-
-    this.sleepy++;
-    this.hunger++;
-
-    this.speak = function() {
-        if(this.species == "Dog") {
+    this.speak = function () {
+        this.sleepy++;
+        this.hunger++;
+        if (this.species === "Dog") {
             return "Woof! That means I love you!";
         }
-        else if(this.species == "Cat") {
-            return "Ugh. Meow.";
+        else if (this.species === "Cat") {
+            return "Ugh. meow.";
         }
-    //    else{}
     }
-
     //The Pet object has an eat method
-
-    this.eat = function() {
-        this.hunger = 0;
+    this.eat = function () {
         this.sleepy++;
-
-        if (this.species == "Cat") {
-            return "Ugh, I guess I'll eat this.";
-
-        }
-        else if (this.species == "Dog"); {
+        this.hunger = 0;
+        if (this.species === "Dog") {
             return "Nom nom nom nom. Yum, thank you for this delicious food! I love you!";
         }
-
-     //   else{}
+        else if (this.species === "Cat") {
+            return "Ugh, I guess I'll eat this."
+        }
     }
-
     //The Pet object has a nap method
-
-    this.nap = function() {
+    this.nap = function () {
         this.sleepy = 0;
         this.awake = false;
         return "Zzzz...";
     }
-
-    //The Pet object has a wakeUp method
-
+     //The Pet object has a wakeUp method
+    this.wakeUp = function () {
+        this.awake = true;
+        this.sleepy = 0;
+        this.hunger = 4;
+        if (this.species === "Dog") {
+            return "I'm awake! Let's eat and then play! I love you!";
+        }
+        else if (this.species === "Cat") {
+            return "Ugh, I'm awake now. Is there anything to eat around here?";
+        }
+    }
+    //The Pet object has a rollOver method
+    this.rollOver = function () {
+        this.hunger++;
+        this.sleepy++;
+        if (this.hunger <= 4 && this.sleepy <= 5) {
+            if (this.species === "Dog") {
+                return "Rolling! Now what? I love you!";
+            }
+            else if (this.species === "Cat") {
+                return "Ugh. I'm a cat. I only roll over if I want to.";
+            }
+        }
+        else if (this.hunger >= 5) {
+            if (this.species === "Dog") {
+                return "I'm hungry! Let's eat first. I love you!";
+            }
+            else if (this.species === "Cat") {
+                return "Ugh. Feed me before I'll even think about it."
+            }
+        }
+        else if (this.sleepy >= 5) {
+            if (this.species === "Dog") {
+                return "Yawn. Let's play after a nap. I love you!";
+            }
+            else if (this.species === "Cat") {
+                return "Ugh. I need a 12 hour nap first."
+            }
+        }
+    }
+   //The Pet object has a fetch method
+    this.fetch = function () {
+        this.hunger++;
+        this.sleepy++;
+        if (this.awake == true && this.hunger < 4 && this.sleepy < 5) {
+            if (this.species === "Dog") {
+                return "Must get the stick! I love you!";
+            }
+            else if (this.species === "Cat") {
+                return "Ugh. I'm a cat. I don't fetch."
+            }
+        }
+        else if (this.hunger >= 5) {
+            if (this.species === "Dog") {
+                return "I'm hungry! Let's eat first. I love you!";
+            }
+            else if (this.species === "Cat") {
+                return "Ugh. Feed me before I'll even think about it."
+            }
+        }
+        else if (this.sleepy >= 5) {
+            if (this.species === "Dog") {
+                return "Yawn. Let's play after a nap. I love you!";
+            }
+            else if (this.species === "Cat") {
+                return "Ugh. I need a 12 hour nap first."
+            }
+        }
+        return "Zzzz...";
+    }
+    //The Pet object has a shake method
+    this.shake = function () {
+        this.hunger++;
+        this.sleepy++;
+        if (this.awake == true && this.hunger < 4 && this.sleepy < 5) {
+            if (this.species === "Cat") {
+                return "Ugh. I'm a cat. I never shake";
+            }
+            else if (this.species === "Dog") {
+                return "Nice to meet you! I love you!";
+            }
+        }
+        else if (this.hunger >= 5) {
+            if (this.species === "Dog") {
+                return "I'm hungry! Let's eat first. I love you!";
+            }
+            else if (this.species === "Cat") {
+                return "Ugh. Feed me before I'll even think about it."
+            }
+        }
+        else if (this.sleepy >= 5) {
+            if (this.species === "Dog") {
+                return "Yawn. Let's play after a nap. I love you!";
+            }
+            else if (this.species === "Cat") {
+                return "Ugh. I need a 12 hour nap first."
+            }
+        }
+        return "Zzzz...";
+    }
 }
